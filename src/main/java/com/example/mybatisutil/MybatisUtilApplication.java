dc00920ee05ed61123bac627c1839194179fe632
梁珑普 <lianglongpu@ewell.cc>
2021-05-16 17:04:46 +0800

    初始化

---
+++ src/main/java/com/example/mybatisutil/MybatisUtilApplication.java
@@ -0,0 +1,13 @@
+package com.example.mybatisutil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisUtilApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisUtilApplication.class, args);
+    }
+
+}
